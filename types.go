6e596815790ee5f2c50524b61762de4226925577
Joel Miller <millerjoel7597@gmail.com>
2026-05-20 15:00:43 +0100

    enhance player structure: add rank, hunter level, skills, and titles attributes

--- types.go
+++ types.go
@@ -81,4 +81,51 @@ type Player struct {
 	ToolDurability int             `json:"tool_durability"`
 	Structures     map[string]bool `json:"structures"`
 	QuestProgress  map[string]int  `json:"quest_progress"`
+	Rank           string          `json:"rank"`
+	HunterLevel    int             `json:"hunter_level"`
+	HunterXP       int             `json:"hunter_xp"`
+	HunterXPToNext int             `json:"hunter_xp_to_next"`
+	HunterRank     string          `json:"hunter_rank"`
+	Kills          int             `json:"kills"`
+	SkillPoints    int             `json:"skill_points"`
+	Titles         []string        `json:"titles"`
+	Skills         []string        `json:"skills"`
+	EquippedSkills []string        `json:"equipped_skills"`
+	SkillSlots     int             `json:"skill_slots"`
+	SkillLevels    map[string]int  `json:"skill_levels"`
+	SkillCooldowns map[string]int  `json:"skill_cooldowns"`
+	CurrentGate    *Gate           `json:"current_gate"`
+}
+
+type Title struct {
+	ID          string
+	Name        string
+	KillsNeeded int
+	PerkDesc    string
+	AttackBonus int
+	HPBonus     int
+}
+
+type Skill struct {
+	ID                string
+	Name              string
+	DmgBonus          int
+	Cooldown          int
+	Desc              string
+	Rank              string // E, D, C, B, A, S
+	Type              string // "active", "passive"
+	Category          string // "attack", "defense", "heal"
+	Level             int
+	UnlockRequirement string
+}
+
+type Gate struct {
+	Rank          string
+	Floors        int
+	MinLevel      int
+	Boss          Monster
+	MonsterCount  int
+	RewardXP      int
+	RewardGold    int
+	Descriptions  []string
 }
